0d7838ad596f222c2b9eb8a34cf5d04106413182
takesoo <takesooon@gmail.com>
2024-03-31 14:47:13 +0900

    create s3

--- lib/image-resize-lambda-stack.ts
+++ lib/image-resize-lambda-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib'
 import { type Construct } from 'constructs'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3'
 
 export class ImageResizeLambdaStack extends cdk.Stack {
   constructor (scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImageResizeLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'ImageResizeLambdaBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
   }
 }
