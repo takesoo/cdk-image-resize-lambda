b95937142e26225dc98c47b1d4e30f823c9ae6f0
takesoo <takesooon@gmail.com>
2024-03-31 13:37:50 +0900

    setup eslint prettier

--- lib/image-resize-lambda-stack.ts
+++ lib/image-resize-lambda-stack.ts
@@ -1,10 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import { type Construct } from 'constructs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ImageResizeLambdaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor (scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
 
     // The code that defines your stack goes here
 
